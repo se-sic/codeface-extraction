9f6e36165272ec6aaba4f2d9d45ba3e24166d43f
Claus Hunsen <hunsen@fim.uni-passau.de>
2016-08-02 19:00:59 +0200

    Completely rewrite extraction process
    The extraction process is now built on top of the class "Extraction",
    which needs to be subclassed for introduction of a new process.
    
    Project-level and range-level extractions are distinguished
    automatically, additionally, only the SQL statement and the reduction
    process needs to be defined.
    
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

--- codeface_extraction/codeface_extraction.py
+++ codeface_extraction/codeface_extraction.py
@@ -5,34 +5,24 @@ This file is able to extract developer--artifact relations from the Codeface dat
 
 import argparse
 import sys
-from os import makedirs
-from os.path import exists as pathexists, abspath
+from os.path import abspath
 
 from codeface.cli import log
 from codeface.configuration import Configuration
 from codeface.dbmanager import DBManager
 
-from author2artifact import *
-from author2file import *
-from authors import *
-from commit2artifact import *
-from commit2file import *
-from revisions import *
-from thread2authors import *
-from commits import *
-from emails import *
+import extractions
 
 
 ##
 # RUN FOR ALL PROJECTS
 ##
 
-def run_extraction(conf, artifacts, resdir):
+def run_extraction(conf, resdir):
     """
     Runs the extraction process for the list of given parameters.
 
     :param conf: the Codeface configuration object
-    :param artifacts: a list of artifacts to extract, e.g., ['Feature', 'FeatureExpression']
     :param resdir: the Codeface results dir, where output files are written
     """
 
@@ -41,27 +31,16 @@ def run_extraction(conf, artifacts, resdir):
     # initialize database manager with given configuration
     dbm = DBManager(conf)
 
-    # get setting for current combination
-    project = conf["project"]
-    project_resdir = conf["project"]
-    revs = conf["revisions"]
-    tagging = conf["tagging"]
-    project_resdir = pathjoin(resdir, project_resdir, tagging)
+    # get all types of extractions, both project-level and range-level
+    __extractions_project, __extractions_range = extractions.get_extractions(dbm, conf, resdir)
 
-    # create results directory
-    if not pathexists(project_resdir):
-        makedirs(project_resdir)
-
-    # extract list of revisions as stored in the database
-    list_of_revisions = get_list_of_revisions(dbm, project, project_resdir)
-    # get the list of authors in this project
-    get_list_of_authors(dbm, project, project_resdir)
-    # get the list of commits in this project
-    get_list_of_commits(dbm, project, tagging, project_resdir)
-    # get the list of emails in this project
-    get_list_of_emails(dbm, project, tagging, project_resdir)
+    # run project-level extractions
+    for extraction in __extractions_project:
+        extraction.run()
 
     # check if list of revisions in database is the same as in the config file
+    revs = conf["revisions"]
+    list_of_revisions = extractions.RevisionExtraction(dbm, conf, resdir).get_list()
     if revs:
         if set(revs) != set(list_of_revisions):
             log.error("List of revisions in configuration file do not match the list stored in the DB! Stopping now.")
@@ -77,32 +56,10 @@ def run_extraction(conf, artifacts, resdir):
         start_rev = revs[i]
         end_rev = revs[i + 1]
 
-        # print (project, tagging, kind, start_rev, end_rev)
+        log.info("%s: Extracting data for version '%s'" % (conf["project"], end_rev))
 
-        # results directory for current revision
-        range_resdir = pathjoin(project_resdir, "{0}-{1}".format(start_rev, end_rev))
-        if not pathexists(range_resdir):
-            makedirs(range_resdir)
-
-        # for all kinds of artifacts that have been analyzed for the current tagging
-        for artifact in artifacts:
-            log.info("%s: Extracting data: %s" % (conf["project"], artifact))
-
-            # extract the author--artifact list
-            get_artifacts_per_author(dbm, project, tagging, end_rev, artifact, range_resdir)
-
-            # get co-changed artifacts (= artifacts per commit)
-            get_cochanged_artifacts(dbm, project, tagging, end_rev, artifact, range_resdir)
-
-        if tagging == 'proximity' or tagging == 'feature':
-            # extract mailing-list analysis (associated with proximity/feature projects!)
-            log.info("%s: Extracting mailing network for version '%s'" % (conf["project"], end_rev))
-            get_mailing_authors(dbm, project, tagging, end_rev, range_resdir)
-
-            # extract author2file mapping (embedded into proximity/feature projects!)
-            log.info("%s: Extracting file network for version '%s'" % (conf["project"], end_rev))
-            get_files_per_author(dbm, project, tagging, end_rev, range_resdir)
-            get_cochanged_files(dbm, project, tagging, end_rev, range_resdir)
+        for extraction in __extractions_range:
+            extraction.run(start_rev, end_rev)
 
 
 def get_parser():
@@ -135,12 +92,7 @@ def run():
     # load configuration
     __conf = Configuration.load(__codeface_conf, __project_conf)
 
-    __tagging2artifacts = {
-        'feature': ['Feature', 'FeatureExpression'],
-        'proximity': ['Function']
-    }
-
-    run_extraction(__conf, __tagging2artifacts[__conf["tagging"]], __resdir)
+    run_extraction(__conf, __resdir)
 
 
 if __name__ == '__main__':
---
+++ codeface_extraction/extractions.py
@@ -0,0 +1,517 @@
+# coding=utf-8
+"""
+This file provides the class 'Extraction' and all of its subclasses.
+"""
+
+import csv_writer
+import itertools
+import os
+
+from codeface.cli import log
+
+
+#
+# GET EXTRACTIONS
+#
+
+
+def get_extractions(dbm, conf, resdir):
+    # all extractions are sublcasses of Extraction:
+    # instantiate them all!
+    __extractions = []
+    for cls in Extraction.__subclasses__():
+        __extractions.append(cls(dbm, conf, resdir))
+
+    # group extractions by "project-levelness"
+    __extractions_grouped = dict(
+        (key, list(e for e in extractions))
+        for key, extractions in
+        itertools.groupby(__extractions, lambda y: y.is_project_level())
+    )
+    __extractions_project = __extractions_grouped[True]
+    __extractions_range = __extractions_grouped[False]
+
+    return __extractions_project, __extractions_range
+
+
+#
+# EXTRACTION
+#
+
+class Extraction(object):
+    """
+    The class Extraction defines the basic extraction process:
+    - run SQL statement,
+    - reduce result table if neccessary,
+    - get output file depending on extraction level, and
+    - write result to output file.
+    """
+
+    # map Codeface taggings to artifact types
+    _tagging2artifacts = {
+        'feature': ['Feature', 'FeatureExpression'],
+        'proximity': ['Function']
+    }
+
+    def __init__(self, dbm, conf, res_dir):
+        """
+        Initialize object variables from parameters.
+
+        :param dbm: the database manager
+        :param conf: the configuration (parsed from YAML file)
+        :param res_dir: the root result directory for the project with :conf
+        """
+
+        self.dbm = dbm
+        self.conf = conf
+        self.project = conf["project"]
+        self.revs = conf["revisions"]
+        self.tagging = conf["tagging"]
+        self.project_res_dir = os.path.join(res_dir, self.project, self.tagging)
+
+        self.file_name = ""
+
+        self.sql = ""
+
+    def is_project_level(self):
+        """Check if this extraction is on project level (i.e., {revision} is not on the SQL statement)."""
+
+        return not ("{revision}" in self.sql)
+
+    def is_generic_extraction(self):
+        """Check if this extraction is generic (i.e., it can be used for several artifacts and, hence,
+        {artifact} is in the file name). """
+
+        return "{artifact}" in self.file_name
+
+    def _run_sql(self, end_revision, entity_type):
+        """
+        Run the SQL statement.
+
+        :param end_revision: the end of an release range
+        :param entity_type: the entity type to be searched
+        :return: the result table
+        """
+
+        self.dbm.doExec(
+            self.sql.format(
+                project=self.project,
+                tagging=self.tagging,
+                revision=end_revision,
+                entity_type=entity_type,
+                artifact=entity_type
+            )
+        )
+        result = self.dbm.doFetchAll()
+        return result
+
+    def _reduce_result(self, result):
+        """
+        Reduces or rearranges the result.
+        :param result: the results as given by the SQL query
+        :return: the reduced (or not reduced) result
+        """
+
+        return result
+
+    def _get_out_file(self, start_revision, end_revision, entity_type):
+        """
+        Return the file path to which the output of this extraction shall be written.
+        This methods ensures that the paths are existent.
+
+        :param start_revision: start of an release range (for range-level extractions)
+        :param end_revision: end of an release range (for range-level extractions)
+        :param entity_type: the artifact to be (probably) encoded in the file name
+        :return: the file name
+        """
+
+        # get result directory
+        dir = os.path.join(
+            self.project_res_dir,
+            "{0}-{1}".format(start_revision, end_revision)
+            if not self.is_project_level()
+            else "",
+        )
+
+        # make sure the result dir exists
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+        # get the file to write inside the result dir
+        outfile = os.path.join(
+            dir,
+            self.file_name.format(
+                artifact=entity_type.lower(),
+                entity_type=entity_type.lower()
+            )
+        )
+
+        # and return it
+        return outfile
+
+    @staticmethod
+    def _write_export_file(lines, outfile):
+        """
+        Write the given lines to the file given by outfile.
+        :param lines: the CSV lines to be written
+        :param outfile: the output file
+        """
+        csv_writer.write_to_csv(outfile, lines)
+
+    def run(self, start_revision=None, end_revision=None):
+        """
+        Runs the extraction.
+
+        :param start_revision: start of an release range (for range-level extractions)
+        :param end_revision: end of an release range (for range-level extractions)
+        """
+
+        artifacts = self._tagging2artifacts[self.tagging]
+        if not self.is_generic_extraction():
+            artifacts = [artifacts[0]]
+
+        for entity_type in artifacts:
+            log.info("%s: %s to %s" %
+                     (self.project,
+                      self.__class__.__name__,
+                      self._get_out_file(start_revision, end_revision, entity_type)
+                      ))
+
+            result = self._run_sql(end_revision, entity_type)
+            lines = self._reduce_result(result)
+            outfile = self._get_out_file(start_revision, end_revision, entity_type)
+            self._write_export_file(lines, outfile)
+
+
+#
+# PROJECT-LEVEL EXTRACTIONS
+#
+
+class AuthorExtraction(Extraction):
+    def __init__(self, dbm, conf, resdir):
+        Extraction.__init__(self, dbm, conf, resdir)
+
+        self.file_name = "authors.list"
+
+        # for subclasses
+        self.sql = """
+                    SELECT pers.id AS id, pers.name AS name
+
+                    FROM project p
+
+                    # add authors/developers/persons
+                    JOIN person pers
+                    ON p.id = pers.projectId
+
+                    # filter for current project
+                    WHERE p.name = '{project}'
+
+                    ORDER BY pers.id ASC
+
+                    # LIMIT 10
+                """
+
+
+class CommitExtraction(Extraction):
+    def __init__(self, dbm, conf, resdir):
+        Extraction.__init__(self, dbm, conf, resdir)
+
+        self.file_name = "commits.list"
+
+        # for subclasses
+        self.sql = """
+                    SELECT c.id, c.authorDate, a.name, a.email1, c.commitHash,
+                           c.ChangedFiles, c.AddedLines, c.DeletedLines, c.DiffSize,
+                           cd.file, cd.entityId, cd.entityType, cd.size
+
+                    FROM project p
+
+                    # get commits for project
+                    JOIN commit c
+                    ON p.id = c.projectId
+
+                    # get commit meta-data
+                    JOIN commit_dependency cd
+                    ON c.id = cd.commitId
+
+                    # add authors/developers/persons
+                    JOIN person a
+                    ON c.author = a.id
+
+                    # filter for current project
+                    WHERE p.name = '{project}'
+                    AND p.analysisMethod = '{tagging}'
+
+                    ORDER BY c.authorDate, a.name, c.id, cd.file, cd.entityId
+
+                    # LIMIT 10
+                """
+
+
+class EmailExtraction(Extraction):
+    def __init__(self, dbm, conf, resdir):
+        Extraction.__init__(self, dbm, conf, resdir)
+
+        self.file_name = "emails.list"
+
+        # for subclasses
+        self.sql = """
+                    SELECT a.name AS authorName, a.email1, m.creationDate, m.subject, m.threadId
+
+                    FROM project p
+
+                    # get mails for project
+                    JOIN mail m
+                    ON p.id = m.projectId
+
+                    # add authors/developers/persons
+                    JOIN person a
+                    ON m.author = a.id
+
+                    # filter for current project
+                    WHERE p.name = '{project}'
+                    AND p.analysisMethod = '{tagging}'
+
+                    ORDER BY authorName, m.creationDate
+
+                    # LIMIT 10
+                """
+
+
+class RevisionExtraction(Extraction):
+    def __init__(self, dbm, conf, resdir):
+        Extraction.__init__(self, dbm, conf, resdir)
+
+        self.file_name = "revisions.list"
+
+        # for subclasses
+        self.sql = """
+                    SELECT tag AS revision
+
+                    FROM project p
+
+                    # add releases
+                    JOIN release_timeline t ON p.id = t.projectId
+
+                    # filter for releases
+                    WHERE p.name = '{project}'
+                    AND t.type = 'release'
+
+                    ORDER BY t.id ASC
+
+                    # LIMIT 10
+                """
+
+    def get_list(self):
+        result = self._run_sql(None, None)
+        lines = self._reduce_result(result)
+        return [rev for (rev,) in lines]
+
+
+#
+# RANGE-LEVEL EXTRACTIONS
+#
+
+class Author2ArtifactExtraction(Extraction):
+    def __init__(self, dbm, conf, resdir):
+        Extraction.__init__(self, dbm, conf, resdir)
+
+        self.file_name = "author2{artifact}.list"
+
+        # for subclasses
+        self.sql = """
+                    SELECT pers.id AS id, pers.name AS name, cd.entityId AS artifact
+
+                    FROM project p
+
+                    # get release range for projects
+                    JOIN release_range r ON p.id = r.projectId
+
+                    # start of range
+                    JOIN release_timeline l1 ON r.releaseStartId = l1.id
+                    # end of range
+                    JOIN release_timeline l2 ON r.releaseEndId = l2.id
+
+                    # add commits for the ranges
+                    JOIN commit c on r.id = c.releaseRangeId
+
+                    # add meta-data for commits
+                    JOIN commit_dependency cd ON c.id = cd.commitId
+
+                    # add authors/developers/persons
+                    JOIN person pers ON c.author = pers.id
+
+                    # filter for current release range and artifact
+                    WHERE p.name = '{project}'
+                    AND p.analysisMethod = '{tagging}'
+                    AND l2.tag = '{revision}'
+                    AND cd.entityType = '{entity_type}'
+
+                    GROUP BY name, artifact ASC
+                    ORDER BY id, artifact ASC
+
+                    # LIMIT 10
+                """
+
+    def _reduce_result(self, result):
+        return [(dev_name, art) for dev_id, dev_name, art in result]
+
+
+class Author2FileExtraction(Extraction):
+    def __init__(self, dbm, conf, resdir):
+        Extraction.__init__(self, dbm, conf, resdir)
+
+        self.file_name = "author2file.list"
+
+        # for subclasses
+        self.sql = """
+                    SELECT pers.id AS id, pers.name AS name, cd.file AS file
+
+                    FROM project p
+
+                    # get release range for projects
+                    JOIN release_range r ON p.id = r.projectId
+
+                    # start of range
+                    JOIN release_timeline l1 ON r.releaseStartId = l1.id
+                    # end of range
+                    JOIN release_timeline l2 ON r.releaseEndId = l2.id
+
+                    # add commits for the ranges
+                    JOIN commit c on r.id = c.releaseRangeId
+
+                    # add meta-data for commits
+                    JOIN commit_dependency cd ON c.id = cd.commitId
+
+                    # add authors/developers/persons
+                    JOIN person pers ON c.author = pers.id
+
+                    # filter for current release range
+                    WHERE p.name = '{project}'
+                    AND p.analysisMethod = '{tagging}'
+                    AND l2.tag = '{revision}'
+
+                    GROUP BY name, file ASC
+                    ORDER BY cd.file, pers.name ASC
+
+                    # LIMIT 10
+                """
+
+    def _reduce_result(self, result):
+        return [(dev_name, filename) for dev_id, dev_name, filename in result]
+
+
+class Commit2ArtifactExtraction(Extraction):
+    def __init__(self, dbm, conf, resdir):
+        Extraction.__init__(self, dbm, conf, resdir)
+
+        self.file_name = "commit2{artifact}.list"
+
+        # for subclasses
+        self.sql = """
+                    SELECT c.id, cd.entityId AS artifact
+
+                    FROM project p
+
+                    # get release range for projects
+                    JOIN release_range r ON p.id = r.projectId
+
+                    # start of range
+                    JOIN release_timeline l1 ON r.releaseStartId = l1.id
+                    # end of range
+                    JOIN release_timeline l2 ON r.releaseEndId = l2.id
+
+                    # add commits for the ranges
+                    JOIN commit c on r.id = c.releaseRangeId
+
+                    # add meta-data for commits
+                    JOIN commit_dependency cd ON c.id = cd.commitId
+
+                    # filter for current release range and artifact
+                    WHERE p.name = '{project}'
+                    AND p.analysisMethod = '{tagging}'
+                    AND l2.tag = '{revision}'
+                    AND cd.entityType = '{entity_type}'
+
+                    ORDER BY c.id, artifact
+
+                    # LIMIT 10
+                """
+
+
+class Commit2FileExtraction(Extraction):
+    def __init__(self, dbm, conf, resdir):
+        Extraction.__init__(self, dbm, conf, resdir)
+
+        self.file_name = "commit2file.list"
+
+        # for subclasses
+        self.sql = """
+                    SELECT c.id, cd.file AS file
+
+                    FROM project p
+
+                    # get release range for projects
+                    JOIN release_range r ON p.id = r.projectId
+
+                    # start of range
+                    JOIN release_timeline l1 ON r.releaseStartId = l1.id
+                    # end of range
+                    JOIN release_timeline l2 ON r.releaseEndId = l2.id
+
+                    # add commits for the ranges
+                    JOIN commit c on r.id = c.releaseRangeId
+
+                    # add meta-data for commits
+                    JOIN commit_dependency cd ON c.id = cd.commitId
+
+                    # filter for current release range
+                    WHERE p.name = '{project}'
+                    AND p.analysisMethod = '{tagging}'
+                    AND l2.tag = '{revision}'
+
+                    ORDER BY c.id, cd.file
+
+                    # LIMIT 10
+                """
+
+
+class Thread2AuthorExtraction(Extraction):
+    def __init__(self, dbm, conf, resdir):
+        Extraction.__init__(self, dbm, conf, resdir)
+
+        self.file_name = "thread2authors.list"
+
+        # for subclasses
+        self.sql = """
+                    SELECT pers.id AS id, pers.name AS name, m.threadId AS thread
+
+                    FROM project p
+
+                    # get release range for projects
+                    JOIN release_range r ON p.id = r.projectId
+
+                    # start of range
+                    JOIN release_timeline l1 ON r.releaseStartId = l1.id
+                    # end of range
+                    JOIN release_timeline l2 ON r.releaseEndId = l2.id
+
+                    # add e-mail data
+                    JOIN mail m ON p.id = m.projectId
+
+                    # add authors/developers/persons
+                    JOIN person pers ON m.author = pers.id
+
+                    # filter for current release range and artifact
+                    WHERE p.name = '{project}'
+                    AND p.analysisMethod = '{tagging}'
+                    AND l2.tag = '{revision}'
+                    AND m.creationDate BETWEEN l1.date AND l2.date
+
+                    ORDER BY m.threadId, m.creationDate ASC
+
+                    #LIMIT 10
+                """
+
+    def _reduce_result(self, result):
+        return [(thread, dev_name) for dev_id, dev_name, thread in result]
