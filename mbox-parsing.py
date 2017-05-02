3517f5eafdb416bccb14687f11c5ba68099bb3d8
Raphael Nömmer <Raphael Nmmer>
2017-05-02 14:39:46 +0100

    Added comments, changed some code style

--- mbox-parsing.py
+++ mbox-parsing.py
@@ -5,6 +5,7 @@ from whoosh.index import create_in, open_dir
 from whoosh.qparser import QueryParser
 from codeface.configuration import Configuration
 from os.path import abspath
+from codeface.cli import log
 import sys
 import csv
 import os.path
@@ -14,52 +15,55 @@ import argparse
 
 def parse(mbox_name, results, include_filepath):
     mbox = mailbox.mbox(mbox_name)
-    commitList = open(results + "/commits.list", 'r')
+    commit_list = open(results + "/commits.list", 'r')
 
-    mySchema = Schema(messageID=ID(stored=True), content=TEXT)
-
-    indexPath = results + "/index"
-    if not os.path.exists(indexPath):
-        print("Creating Index in results folder for text search.")
-        os.mkdir(indexPath)
-        ix = create_in(indexPath, mySchema)
-        ix = open_dir(indexPath)
+    # The index for Whoosh full text search is being created. If an index already exists this step won't be performed
+    # TODO can lead to problems if the index needs to be updated or creation was aborted.
+    my_schema = Schema(messageID=ID(stored=True), content=TEXT)
+    index_path = results + "/index"
+    if not os.path.exists(index_path):
+        log.info("Creating Index in results folder for text search.")
+        os.mkdir(index_path)
+        ix = create_in(index_path, my_schema)
+        ix = open_dir(index_path)
         writer = ix.writer()
         for message in mbox:
             writer.add_document(messageID=unicode(message['message-id']), content=getbody(message))
         writer.commit()
-        print("Index created, parsing will begin now.")
-
+        log.info("Index created, parsing will begin now.")
     else:
-        print("Index has already been created, parsing will begin right away.")
+        log.info("Index has already been created, parsing will begin right away.")
         ix = open_dir("index")
 
-    commitsSeperated = []
+    commits_seperated = []
     commits = []
-    commitSet = set()
-    for line in commitList:
+    commit_set = set()
+    for line in commit_list:
         commits.append(line)
-
     for commit in commits:
-        commitSeperated = str.split(commit, ';')
-        commitSet.add((commitSeperated[9], commitSeperated[10]))
-        commitsSeperated.append(commitSeperated)
-    if include_filepath :
-        myFile = open(results + "mboxParsing_filepath.csv", 'w')  # , newline=''
-    else :
-        myFile = open(results + "mboxParsing.csv", 'w')  # , newline=''
-    wr = csv.writer(myFile, delimiter=';')
+        commit_seperated = str.split(commit, ';')
+        commit_set.add((commit_seperated[9], commit_seperated[10]))
+        commits_seperated.append(commit_seperated)
+    if include_filepath:
+        my_file = open(results + "mboxParsing_filepath.csv", 'w')  # , newline=''
+    else:
+        my_file = open(results + "mboxParsing.csv", 'w')  # , newline=''
+    wr = csv.writer(my_file, delimiter=';')
     wr.writerow(('file', 'function', 'message_id'))
+    # Paralell execution call for the main text search.
     num_cores = multiprocessing.cpu_count()
-    csvData = Parallel(n_jobs=num_cores)(delayed(execute)(commit, mySchema, ix, include_filepath) for commit in commitSet)
-    print("Parsing done writing to file commences.")
-    for entry in csvData:
+    csv_data = Parallel(n_jobs=num_cores - 1)(
+        delayed(execute)(commit, my_schema, ix, include_filepath) for commit in commit_set)
+    log.info("Parsing done writing to file commences.")
+    for entry in csv_data:
         for row in entry:
             wr.writerow(row)
-    myFile.close()
+    my_file.close()
+    log.info("Writing done and file closed.")
 
 
-def getbody(message):  # getting plain text 'email body'
+# Getting plain text 'email body'
+def getbody(message):
     body = None
     if message.is_multipart():
         for part in message.walk():
@@ -74,23 +78,25 @@ def getbody(message):  # getting plain text 'email body'
     return unicode(body, errors="replace")
 
 
-def execute(commit, mySchema, ix, include_filepath):
+# Executes the search for one artifact.
+def execute(commit, my_schema, ix, include_filepath):
     result = []
     with ix.searcher() as searcher:
-        parser = QueryParser("content", schema=mySchema)
-        if include_filepath :
-            myquery = parser.parse(commit[0] + " AND " +  commit[1])
-        else :
-            myquery = parser.parse(commit[1])
-        queryResult = searcher.search(myquery, terms=True)
-        for r in queryResult:
-            touple = (commit[0][1:-1], commit[1][1:-1], r["messageID"])
-            result.append(touple)
-        print("Artefact " + commit[0][1:-1] + ", " + commit[1][1:-1] + " done!")
+        query_parser = QueryParser("content", schema=my_schema)
+        if include_filepath:
+            my_query = query_parser.parse(commit[0] + " AND " + commit[1])
+        else:
+            my_query = query_parser.parse(commit[1])
+        query_result = searcher.search(my_query, terms=True)
+        for r in query_result:
+            result_tuple = (commit[0][1:-1], commit[1][1:-1], r["messageID"])
+            result.append(result_tuple)
+        log.info("Artifact " + commit[0][1:-1] + ", " + commit[1][1:-1] + " done!")
         return result
 
 
 if __name__ == "__main__":
+    # Get all needed paths and argument for the method call.
     parser = argparse.ArgumentParser(prog='codeface', description='Codeface extraction')
     parser.add_argument('-c', '--config', help="Codeface configuration file", default='codeface.conf')
     parser.add_argument('-p', '--project', help="Project configuration file", required=True)
@@ -110,4 +116,5 @@ if __name__ == "__main__":
     __conf = Configuration.load(__codeface_conf, __project_conf)
 
     for ml in __conf["mailinglists"]:
-        parse(__maildir + "/" + ml["name"] + ".mbox", __resdir + "/" + __project + "_" + __tagging + "/" + __tagging, args.filepath)
+        parse(__maildir + "/" + ml["name"] + ".mbox", __resdir + "/" + __project + "_" + __tagging + "/" + __tagging,
+              args.filepath)
