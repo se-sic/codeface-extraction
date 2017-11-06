043685d3d0128069e5897be6f3b1577e7abb27b3
Claus Hunsen <hunsen@fim.uni-passau.de>
2017-11-06 13:20:50 +0100

    Refactor issue processing
    With this refactoring, the issue processing now uses Codeface internals
    to contact the ID service and uses the common package for writing output
    files. Additionally, the code is cleaned up and documented.
    
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

--- issue_processing/issue_processing.py
+++ issue_processing/issue_processing.py
@@ -1,50 +1,83 @@
 import argparse
-import csv
 import httplib
 import json
 import os
 import sys
 import urllib
 
+from codeface.cli import log
+from codeface.cluster.idManager import idManager
 from codeface.configuration import Configuration
 from codeface.dbmanager import DBManager
 
+from csv_writer import csv_writer
+
 
 def run():
-    # Get all needed paths and argument for the method call.
+    # get all needed paths and argument for the method call.
     parser = argparse.ArgumentParser(prog='codeface', description='Codeface extraction')
     parser.add_argument('-c', '--config', help="Codeface configuration file", default='codeface.conf')
     parser.add_argument('-p', '--project', help="Project configuration file", required=True)
     parser.add_argument('resdir', help="Directory to store analysis results in")
 
+    # parse arguments
     args = parser.parse_args(sys.argv[1:])
     __codeface_conf, __project_conf = map(os.path.abspath, (args.config, args.project))
 
+    # create configuration
     __conf = Configuration.load(__codeface_conf, __project_conf)
-    __resdir = os.path.abspath(os.path.join(args.resdir, __conf['repo'] + "_issues"))
 
-    _dbm = DBManager(__conf)
-    project_id = _dbm.getProjectID(__conf["project"], __conf["tagging"])
+    # get source and results folders
+    __srcdir = os.path.abspath(os.path.join(args.resdir, __conf['repo'] + "_issues"))
+    __resdir = os.path.abspath(os.path.join(args.resdir, __conf['project'], __conf["tagging"]))
 
-    issues = load(__resdir)
+    # run processing of issue data:
+    # 1) load the list of issues
+    issues = load(__srcdir)
+    # 2) re-format the issues
     issues = reformat(issues)
-    issues = insert_user_data(issues, project_id, __conf)
+    # 3) update user data with Codeface database
+    issues = insert_user_data(issues, __conf)
+    # 4) dump result to disk
     print_to_disk(issues, __resdir)
 
 
-def load(file_path):
-    with open(os.path.join(file_path, "issue_processing.json")) as issues_file:
-        data = json.load(issues_file)
-    return data
+def load(source_folder):
+    """Load issues from disk.
+
+    :param source_folder: the folder where to find 'issues.json'
+    :return: the loaded issue data
+    """
+
+    srcfile = os.path.join(source_folder, "issues.json")
+    log.devinfo("Loading issues from file '{}'...".format(srcfile))
+
+    # check if file exists and exit early if not
+    if not os.path.exists(srcfile):
+        log.devinfo("Issue file '{}' does not exist! Exiting early...".format(srcfile))
+        sys.exit(-1)
+
+    with open(srcfile) as issues_file:
+        issue_data = json.load(issues_file)
+
+    return issue_data
 
 
-def reformat(issues):
-    for issue in issues:
-        if issue["closed_at"] is None:
-            issue["closed_at"] = ""
+def reformat(issue_data):
+    """Re-arrange issue data structure.
 
-        # add created event
-        dates = dict()
+    :param issue_data: the issue data to re-arrange
+    :return: the re-arranged issue data
+    """
+
+    log.devinfo("Re-arranging issues...")
+
+    # re-process all issues
+    for issue in issue_data:
+        # temporary container for references
+        comments = dict()
+
+        # initialize event
         created_event = dict()
         created_event["user"] = issue["user"]
         created_event["created_at"] = issue["created_at"]
@@ -54,98 +87,134 @@ def reformat(issues):
         # add event name to comment and add reference target
         for comment in issue["commentsList"]:
             comment["event"] = "commented"
-            dates[comment["created_at"]] = comment
             comment["ref_target"] = ""
+            # cache comment by date to resolve/re-arrange references later
+            comments[comment["created_at"]] = comment
 
         # add reference target to events
         for event in issue["eventsList"]:
             event["ref_target"] = ""
-            if event["created_at"] in dates:
-                check_event(event, dates[event["created_at"]])
+            # if event collides with a comment
+            if event["created_at"] in comments:
+                comment = comments[event["created_at"]]
+                # if someone gets mentioned or subscribed by someone else in a comment,
+                # re-write the reference
+                if (event["event"] == "mentioned" or event["event"] == "subscribed") and \
+                                comment["event"] == "commented":
+                    event["ref_target"] = event["user"]
+                    event["user"] = comment["user"]
+
         # merge events and comment lists
         issue["eventsList"] = issue["commentsList"] + issue["eventsList"]
 
+        # add 'closed_at' information if not present yet
+        if issue["closed_at"] is None:
+            issue["closed_at"] = ""
+
         # remove events without user
-        event_list = []
-        for event in issue["eventsList"]:
-            if not (event["user"] is None or event["ref_target"] is None):
-                event_list.append(event)
-        issue["eventsList"] = event_list
+        issue["eventsList"] = [event for event in issue["eventsList"] if
+                               not (event["user"] is None or event["ref_target"] is None)]
 
-    return issues
+    return issue_data
 
 
-def check_event(event1, comment):
-    if (event1["event"] == "mentioned" or event1["event"] == "subscribed") and comment["event"] == "commented":
-        event1["ref_target"] = event1["user"]
-        event1["user"] = comment["user"]
-    return event1
+def insert_user_data(issues, conf):
+    """Insert user data into database ad update issue data.
 
+    :param issues: the issues to retrieve user data from
+    :param conf: the project configuration
+    :return: the updated issue data
+    """
 
-def insert_user_data(issues, project_id, __conf):
+    log.devinfo("Re-arranging issues...")
+
+    # create buffer for users
     user_buffer = dict()
+    # open database connection
+    dbm = DBManager(conf)
+    # open ID-service connection
+    idservice = idManager(dbm, conf)
+
+    def get_user_string(name, email):
+        if not email or email is None:
+            return "{name}".format(name=name)
+            # return "{name} <{name}@default.com>".format(name=name)  # for debugging only
+        else:
+            return "{name} <{email}>".format(name=name, email=email)
+
+    def get_or_update_user(user, buffer_db=user_buffer):
+        # fix encoding for name and e-mail address
+        if user["name"] is not None:
+            name = unicode(user["name"]).encode("utf-8")
+        else:
+            name = unicode(user["username"]).encode("utf-8")
+        mail = unicode(user["email"]).encode("utf-8")
+        # construct string for ID service and send query
+        user_string = get_user_string(name, mail)
+
+        # check buffer to reduce amount of DB queries
+        if user_string in buffer_db:
+            log.devinfo("Returning user '{}' from buffer.".format(user_string))
+            return buffer_db[user_string]
+
+        # get person information from ID service
+        log.devinfo("Passing user '{}' to ID service.".format(user_string))
+        idx = idservice.getPersonID(user_string)
+
+        # update user data with person information from DB
+        person = idservice.getPersonFromDB(idx)
+        user["email"] = person["email1"]  # column 'email1'
+        user["name"] = person["name"]  # column 'name'
+        user["id"] = person["id"]  # column 'id'
+
+        # add user information to buffer
+        # user_string = get_user_string(user["name"], user["email"]) # update for
+        buffer_db[user_string] = user
+
+        return user
+
     for issue in issues:
+        # check database for issue author
+        issue["user"] = get_or_update_user(issue["user"])
+
         # check database for event authors
         for event in issue["eventsList"]:
-            if event["user"]["username"] not in user_buffer:
-                event["user"] = check_user(event["user"], project_id, __conf)
-                user_buffer[event["user"]["username"]] = event["user"]
-            else:
-                event["user"] = user_buffer[event["user"]["username"]]
-            if event["ref_target"] != "" and event["ref_target"]["username"] not in user_buffer:
-                event["ref_target"] = check_user(event["ref_target"], project_id, __conf)
-                user_buffer[event["ref_target"]["username"]] = event["ref_target"]
-            elif event["ref_target"] != "":
-                event["ref_target"] = user_buffer[event["ref_target"]["username"]]
+            # get the event user from the DB
+            event["user"] = get_or_update_user(event["user"])
+            # get the reference-target user from the DB if needed
+            if event["ref_target"] != "":
+                event["ref_target"] = get_or_update_user(event["ref_target"])
 
-        # check database for issue authors
-        if issue["user"]["username"] not in user_buffer:
-            issue["user"] = check_user(issue["user"], project_id, __conf)
-            user_buffer[issue["user"]["username"]] = issue["user"]
-        else:
-            issue["user"] = user_buffer[issue["user"]["username"]]
     return issues
 
 
-def check_user(user, project_id, __conf):
-    if user["name"] is not None:
-        name = unicode(user["name"]).encode("utf-8")
-    else:
-        name = unicode(user["username"]).encode("utf-8")
-    mail = unicode(user["email"]).encode("utf-8")
+def print_to_disk(issues, results_folder):
+    """Print issues to file 'issues.list' in result folder
 
-    conn = httplib.HTTPConnection(__conf['idServiceHostname'], __conf['idServicePort'])
-    headers = {"Content-type": "application/x-www-form-urlencoded; charset=utf-8", "Accept": "text/plain"}
-    params = urllib.urlencode({"projectID": project_id, "name": name, "email": mail})
-    conn.request("POST", "/post_user_id", params, headers)
-    response = conn.getresponse()
-    user_id = response.read()
-    user_id = json.loads(user_id)
-    if "error" in user_id:
-        mail = name + "@default.com"
-        params = urllib.urlencode({"projectID": project_id, "name": name, "email": mail})
-        conn.request("POST", "/post_user_id", params, headers)
-        response = conn.getresponse()
-        user_id = json.loads(response.read())
-    url = "/getUser/" + str(user_id["id"])
-    conn.request("GET", url, headers=headers)
-    response = conn.getresponse()
-    db_user = json.loads(response.read())[0]
+    :param issues: the issues to dump
+    :param results_folder: the folder where to place 'issues.list' output file
+    """
 
-    user["email"] = db_user["email1"]
-    user["name"] = db_user["name"]
-    conn.close()
-    return user
+    # construct path to output file
+    output_file = os.path.join(results_folder, "issues.list")
+    log.devinfo("Dumping output in file '{}'...".format(output_file))
 
+    # construct lines of output
+    lines = []
+    for issue in issues:
+        for event in issue["eventsList"]:
+            lines.append((
+                issue["number"],
+                issue["state"],
+                issue["created_at"],
+                issue["closed_at"],
+                issue["isPullRequest"],
+                event["user"]["name"],
+                event["user"]["email"],
+                event["created_at"],
+                "" if event["ref_target"] == "" else event["ref_target"]["name"],
+                event["event"]
+            ))
 
-def print_to_disk(issues, file_path):
-    with open(file_path + "/issue_processing.list", 'wb') as output_file:
-        wr = csv.writer(output_file, delimiter=';', lineterminator='\n', quoting=csv.QUOTE_NONNUMERIC)
-        for issue in issues:
-            for event in issue["eventsList"]:
-                wr.writerow((
-                    issue["number"], issue["state"], issue["created_at"], issue["closed_at"], issue["isPullRequest"],
-                    (event["user"]["name"]).encode("utf-8"), (event["user"]["email"]).encode("utf-8"),
-                    event["created_at"],
-                    "" if event["ref_target"] == "" else (event["ref_target"]["name"]).encode("utf-8"),
-                    event["event"]))
+    # write to output file
+    csv_writer.write_to_csv(output_file, lines)
