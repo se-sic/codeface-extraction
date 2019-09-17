se-sic/codeface-extraction

commit d95967f7e18b367359d417fc3cdb51f08a04a85b

Fix name encoding when extracting author, commit, and e-mail data
In the database, there are sometimes ill-encoded names originating from
e-mail data using specific header encodings. When extracting author,
commit, and e-mail data from the database, we still extract those
ill-encoded names.

In this commit, we post-process names after extracting them from the
database and fix the encoding if possible, to get well-readable names in
the resulting .list files.

Props to @nischikata for making us aware of how this can be fixed and
for providing a suitable draft of the eventual solution.

Signed-off-by: Thomas Bock <bockthom@fim.uni-passau.de>

codeface_extraction/extractions.py
@@ -26,6 +26,7 @@ import os
 import unicodedata
 import re
 from ftfy import fix_encoding
+from email.header import decode_header, make_header
 
 from codeface.cli import log
 from codeface.util import gen_range_path
@@ -250,6 +251,11 @@ class AuthorExtraction(Extraction):
                     # LIMIT 10
                 """
 
+    def _reduce_result(self, result):
+        # fix name encoding
+        return [(id, fix_name_encoding(name), email)
+                for (id, name, email) in result]
+
 
 class CommitExtraction(Extraction):
     def __init__(self, dbm, conf, resdir, csv_writer):
@@ -287,6 +293,17 @@ class CommitExtraction(Extraction):
 
                     # LIMIT 10
                 """
+
+    def _reduce_result(self, result):
+        # fix name encoding
+        return [(id, authorDate, fix_name_encoding(authorName), authorEmail,
+                 commitDate, fix_name_encoding(committerName), committerEmail,
+                 commitHash, changedFiles, addedLines, deletedLines, diffSize,
+                 file, entityId, entityType, size)
+                for (id, authorDate, authorName, authorEmail,
+                     commitDate, committerName, committerEmail,
+                     commitHash, changedFiles, addedLines, deletedLines, diffSize,
+                     file, entityId, entityType, size) in result]
 
 
 class CommitMessageExtraction(Extraction):
@@ -379,6 +396,13 @@ class EmailExtraction(Extraction):
                     # LIMIT 10
                 """
 
+    def _reduce_result(self, result):
+        # fix name encoding
+        return [(fix_name_encoding(name), email, messageId, creationDate, creationDateOffset,
+                 subject, threadId)
+                for (name, email, messageId, creationDate, creationDateOffset,
+                     subject, threadId) in result]
+
 
 class RevisionExtraction(Extraction):
     def __init__(self, dbm, conf, resdir, csv_writer):
@@ -439,6 +463,11 @@ class AuthorRangeExtraction(Extraction):
                     # LIMIT 10
                 """
 
+    def _reduce_result(self, result):
+        # fix name encoding
+        return [(id, fix_name_encoding(name), email)
+                for (id, name, email) in result]
+
 
 class CommitRangeExtraction(Extraction):
     """This is basically the CommitExtraction, but for one range only."""
@@ -487,6 +516,17 @@ class CommitRangeExtraction(Extraction):
 
                     # LIMIT 10
                 """
+
+    def _reduce_result(self, result):
+        # fix name encoding
+        return [(id, authorDate, fix_name_encoding(authorName), authorEmail,
+                 committerDate, fix_name_encoding(committerName), committerEmail,
+                 commitHash, changedFiles, addedLines, deletedLines, diffSize,
+                 file, entityId, entityType, size)
+                for (id, authorDate, authorName, authorEmail,
+                     commitDate, committerName, committerEmail,
+                     commitHash, changedFiles, addedLines, deletedLines, diffSize,
+                     file, entityId, entityType, size) in result]
 
 
 class CommitMessageRangeExtraction(Extraction):
@@ -567,6 +607,13 @@ class EmailRangeExtraction(Extraction):
                     # LIMIT 10
                 """
 
+    def _reduce_result(self, result):
+        # fix name encoding
+        return [(fix_name_encoding(name), email, messageId, creationDate, creationDateOffset,
+                 subject, threadId)
+                for (name, email, messageId, creationDate, creationDateOffset,
+                     subject, threadId) in result]
+
 
 class FunctionImplementationRangeExtraction(Extraction):
     def __init__(self, dbm, conf, resdir, csv_writer):
@@ -634,3 +681,29 @@ def fix_characters_in_string(text):
     new_text = u"".join(ch if unicodedata.category(ch)[0] != "C" else " " for ch in new_text.decode("unicode-escape"))
 
     return new_text
+
+
+def fix_name_encoding(name):
+    """
+    Fix encoding of names (originating from e-mail headers).
+    :param name: expects a name string extracted from the database
+    :return: unicode string of the name (correctly encoded)
+    """
+
+    # encode utf-8
+    name = name.encode('utf-8')
+
+    # find out character set of the encoded name
+    info = decode_header(str(name))
+
+    try:
+        # Apply correct encoding and return unicode string
+        return unicode(make_header(info))
+    except UnicodeDecodeError:
+        # Undo utf-8 encoding and return unicode string
+        return unicode(name.decode('utf-8'))
+    except LookupError:
+        # Encoding not found, return string as is
+        return name
+    return name
+

commit 5b6504d7b1de45a7c9f0c946803461fa305bf353

Fix undefined variable in 'CommitRangeExtraction'
The variable 'committerDate' is not defined in the context of
'CommitRangeExtraction::_reduce_result'. When enabling '--range' in the
CLI, an error is produced and the extraction crashes. This is a
regression introduced by commit d95967f7e18b367359d417fc3cdb51f08a04a85b
and which has been missed in the review for PR #23.

The mistake is fixed by accessing the wanted variable 'commitDate'.

Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

codeface_extraction/extractions.py
@@ -520,7 +520,7 @@ class CommitRangeExtraction(Extraction):
     def _reduce_result(self, result):
         # fix name encoding
         return [(id, authorDate, fix_name_encoding(authorName), authorEmail,
-                 committerDate, fix_name_encoding(committerName), committerEmail,
+                 commitDate, fix_name_encoding(committerName), committerEmail,
                  commitHash, changedFiles, addedLines, deletedLines, diffSize,
                  file, entityId, entityType, size)
                 for (id, authorDate, authorName, authorEmail,