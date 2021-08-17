71617a8fde149647742f6b3d0b655c65b91c803a
Thomas Bock <bockthom@cs.uni-saarland.de>
2021-08-17 18:49:21 +0200

    [bot processing] Enable adding bots from project-independent list of known bots
    We now can have a global, project-independent list of known bots. This file has
    the name "known_github_bots.list" and is located in the "configurations"
    directory. The file just has to contain **GitHub usernames**, one username per
    row. All usernames in this file are treaded as bots then.
    
    When processing the bot data we have extracted from a project, we check whether
    also any of these known bots does occur in the GitHub issue data of the project.
    If so, we add the known bot to the list of bots. If the username was already
    present in the list of bots before, the prediction value is updated accordingly.
    
    Signed-off-by: Thomas Bock <bockthom@cs.uni-saarland.de>

--- bot_processing/bot_processing.py
+++ bot_processing/bot_processing.py
@@ -48,23 +48,30 @@ def run():
     __srcdir = os.path.abspath(os.path.join(args.resdir, __conf['repo'] + "_issues"))
     __resdir = os.path.abspath(os.path.join(args.resdir, __conf['project'], __conf["tagging"]))
 
+    # get folder that contains known bots file
+    # (the known bots file is the file in which known bots have been added manually and project independent)
+    __confdir = os.path.join(args.resdir, os.path.dirname(args.config))
+    __known_bots_file = os.path.abspath(os.path.join(__confdir, "known_github_bots.list"))
+
     # run processing of bot data:
     # 1) load bot data
-    bots = load_bot_data(os.path.join(__srcdir, "bots.csv"))
+    bots = load_bot_data(os.path.join(__srcdir, "bots.csv"), header = True)
     # 2) load user data
     users = load_user_data(os.path.join(__srcdir, "usernames.list"))
-    # 3) update bot data with user data
-    bots = add_user_data(bots, users)
+    # 3) update bot data with user data and additionally add known bots if they occur in the project
+    bots = add_user_data(bots, users, __known_bots_file)
     # 4) dump result to disk
     print_to_disk(bots, __resdir)
 
     log.info("Bot processing complete!")
 
 
-def load_bot_data(bot_file):
-    """Read list of detected bots and human users from disk.
+def load_bot_data(bot_file, header = True):
+    """
+    Read list of detected bots and human users from disk.
 
     :param bot_file: the file which contains information about bot and humans users
+    :param header: whether the file to be read contains a header or not (default: True)
     :return: the read bot data
     """
 
@@ -77,8 +84,9 @@ def load_bot_data(bot_file):
 
     bot_data = csv_writer.read_from_csv(bot_file, delimiter=',')
 
-    # remove first line which contains column headers
-    bot_data = bot_data[1:]
+    if header:
+        # remove first line which contains column headers
+        bot_data = bot_data[1:]
 
     return bot_data
 
@@ -103,9 +111,57 @@ def load_user_data(user_data_file):
     return user_data
 
 
-def add_user_data(bot_data, user_data):
+def check_with_known_bot_list(known_bots_file, bot_data, user_data, bot_data_reduced):
+    """
+    Check whether there are known bots occurring in the project. If so, add them to the bots list
+    or update the bots list accordingly.
+
+    :param known_bots_file: the file path to the list of known bot data
+    :param bot_data: the bot data originating from the bot prediction
+    :param user_data: a dictionary from the issue data which maps GitHub usernames to authors
+    :param bot_data_reduced: the bot data after mapping GitHub user names to authors
+    :return: the bot data as provided in param 'bot_data_reduced' but possibly enrichted with
+             additional bots (if occurring) or updated bots
+    """
+
+    # Read the list of known bots
+    known_bots = load_bot_data(known_bots_file, header = False)
+
+    # Get the GitHub usernames of the bots predicted to be a bot
+    predicted_bots = [bot[0] if len(bot) > 0 else "" for bot in bot_data]
+
+    for bot in known_bots:
+
+        # (1) check if a known bot occurs in the GitHub issue data but has not been predicted
+        if bot[0] not in predicted_bots and bot[0] in user_data:
+
+            # add the known bot as a bot to the bots list
+            additional_bot = dict()
+            additional_bot["user"] = user_data[bot[0]]
+            additional_bot["prediction"] = "Bot"
+            bot_data_reduced.append(additional_bot)
+            log.info("Add known bot '{}' to bot data.".format(additional_bot["user"]))
+
+        # (2) handle known bots that are already present in the bots list
+        elif bot[0] in predicted_bots and bot[0] in user_data:
+
+            # make sure that this bot has also been predicited to be bot
+            for predicted_bot in bot_data_reduced:
+                if predicted_bot["user"] == user_data[bot[0]]:
+                    predicted_bot["prediction"] = "Bot"
+                    log.info("Mark user '{}' as bot in the bot data.".format(user_data[bot[0]]))
+                    break
+
+    # return the updated bot data
+    return bot_data_reduced
+
+
+def add_user_data(bot_data, user_data, known_bots_file):
     """
     Add user data to bot data, i.e., replace username by name and e-mail.
+    In addition, check in the global bots list whether there are authors in the projects which are
+    globally known to be bots. If so, add them to the bots list and update the resulting bots list
+    accordingly.
 
     :param bot_data: the list of detected bot/human users
     :param user_data: the list of usernames to retrieve user data from
@@ -142,6 +198,9 @@ def add_user_data(bot_data, user_data):
             bot_data_reduced.append(bot_reduced)
         else:
             log.warn("User '{}' in bot data does not occur in GitHub user data. Remove user...".format(user[0]))
+
+    # check whether known GitHub bots occur in the GitHub issue data and, if so, update the bot data accordingly
+    bot_data_reduced = check_with_known_bot_list(known_bots_file, bot_data, user_buffer, bot_data_reduced)
 
     return bot_data_reduced
 
